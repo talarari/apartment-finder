826d78ab277b37b7c5ac0ed9dfd9ef918c3ff395
talarari <talarari@gmail.com>
2016-07-07 13:53:57 +0300

    my prefs

--- query.js
+++ query.js
@@ -1,24 +1,22 @@
 module.exports = {
   searchArea: [
-    {latitude: 32.078284, longitude: 34.801168},
-    {latitude: 32.079157, longitude: 34.815073},
-    {latitude: 32.068174, longitude: 34.814472},
-    {latitude: 32.066501, longitude: 34.808636},
-    {latitude: 32.063810, longitude: 34.796877},
-    {latitude: 32.068538, longitude: 34.795761},
-    {latitude: 32.071593, longitude: 34.797049},
-    {latitude: 32.070865, longitude: 34.799881},
-    {latitude: 32.076684, longitude: 34.802198},
+    {latitude:32.068029,longitude:34.761171} 
+    {latitude:32.060682,longitude:34.770441} 
+    {latitude:32.062792,longitude:34.778166} 
+    {latitude:32.066356,longitude:34.783831} 
+    {latitude:32.081411,longitude:34.781513} 
+    {latitude:32.087883,longitude:34.766235} 
+    {latitude:32.068538,longitude:34.761343}
   ],
-  minimumPublishDate: new Date('2/27/2016'),
+  minimumPublishDate: new Date('7/7/2016'),
   apartment: {
-    FromPrice: 1500000,
-    ToPrice: 2500000,
-    AreaID:2,     // 1:TA, 2:center, 3:Ramat-Gan-Givatatyim, 48:TA center, 19:HaSharon
-    SubCatID:1,   // buy=1,rent=2
-    FromRooms:3.5,
-    ToRooms:4.5,
-    FromSQM:80,
-    ToSQM:120
+    FromPrice: 5500,
+    ToPrice: 6500,
+    AreaID:48,     // 1:TA, 2:center, 3:Ramat-Gan-Givatatyim, 48:TA center, 19:HaSharon
+    SubCatID:2,   // buy=1,rent=2
+    FromRooms:2,
+    ToRooms:3,
+    FromSQM:50,
+    ToSQM:100
   }
 }

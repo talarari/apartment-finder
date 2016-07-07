aa4e84f99a09024a1932e113a16f60be49cbedda
talarari <talarari@gmail.com>
2016-07-07 13:56:07 +0300

    fixed bad json

--- query.js
+++ query.js
@@ -1,11 +1,11 @@
 module.exports = {
   searchArea: [
-    {latitude:32.068029,longitude:34.761171} 
-    {latitude:32.060682,longitude:34.770441} 
-    {latitude:32.062792,longitude:34.778166} 
-    {latitude:32.066356,longitude:34.783831} 
-    {latitude:32.081411,longitude:34.781513} 
-    {latitude:32.087883,longitude:34.766235} 
+    {latitude:32.068029,longitude:34.761171}, 
+    {latitude:32.060682,longitude:34.770441}, 
+    {latitude:32.062792,longitude:34.778166}, 
+    {latitude:32.066356,longitude:34.783831}, 
+    {latitude:32.081411,longitude:34.781513}, 
+    {latitude:32.087883,longitude:34.766235}, 
     {latitude:32.068538,longitude:34.761343}
   ],
   minimumPublishDate: new Date('7/7/2016'),
